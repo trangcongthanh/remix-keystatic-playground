ceef8b694e1f389851711b3a97c9dda05bbdfa23
Thành Trang <14819342+trangcongthanh@users.noreply.github.com>
2024-06-16 01:00:31 +0700

    fix rnv

--- app/routes/api.keystatic.$.tsx
+++ app/routes/api.keystatic.$.tsx
@@ -2,5 +2,13 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
 import { handleLoader } from "@keystatic/remix/api";
 import { config } from "../keystatic/configs";
 
-export const loader: LoaderFunction = (args) => handleLoader({ config }, args);
-export const action: ActionFunction = (args) => handleLoader({ config }, args);
+const env = {
+  clientId: import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: import.meta.env.KEYSTATIC_SECRET,
+};
+
+export const loader: LoaderFunction = (args) =>
+  handleLoader({ config, ...env }, args);
+export const action: ActionFunction = (args) =>
+  handleLoader({ config, ...env }, args);
